392aa9573925abaee2cccdbea755d15395b5a6f6
Marcia Moss <marcia_moss@hotmail.com>
2022-11-23 18:58:14 -0500

    page not found added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useLocation } from "react-router-dom";
 import { connect } from 'react-redux';
 
 import { authChange } from '../../actions';
 import './Header.css';
 import ErrorModal from '../ErrorModal/ErrorModal';
+import PageNotFound from '../PageNotFound/PageNotFound';
 
-const Header = ({userId, userName, authChange, showError, closeModal}) => 
-  (
+const Header = ({userId, userName, authChange, showError, closeModal}) => {
+  const location = useLocation();
+  const showHeader = (["/", "/music", "/movies", "/books", "/videos"].filter(r => r===location.pathname)).length>0;
+  if (!showHeader) {
+    return <PageNotFound/>;
+  }
+  return (
     <div className="ui top fixed inverted massive menu">
       {(showError && !closeModal) ?
         <ErrorModal/>
@@ -24,7 +31,7 @@ const Header = ({userId, userName, authChange, showError, closeModal}) =>
       }
     </div>
   );
-
+}
 
 const mapStateToProps = state => ({ userId: state.auth.userId, userName: state.auth.userName, showError: state.auth.showError, closeModal: state.errorModal.close})
 
---
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./PageNotFound.css";
+import { Message } from 'semantic-ui-react';
+
+const PageNotFound = () => (
+  <Message negative size='massive' className="page-not-found">PAGE NOT FOUND</Message>
+);
+
+export default PageNotFound;
